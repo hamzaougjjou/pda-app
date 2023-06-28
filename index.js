59e60ff9aa998644073e76022fd46ae5ed91702f
hamzaougjjou <hamzaougjjou04@gmail.com>
2023-06-28 15:18:40 +0100

    update a package json yyy

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
+
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');;
+const multer = require('multer');
+const path = require("path");
 const app = express();
 require('dotenv').config()
 
@@ -10,15 +15,55 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// connect to mySql database
+var connection = mysql.createConnection({
+    host: "b8urbhjjqawd9w3ukybw-mysql.services.clever-cloud.com" ,
+    user: "ukv9c9vpdg7hiwvg",
+    password: "jBbszbEUjWB9d9tn9Hy9",
+    database: "b8urbhjjqawd9w3ukybw"
+});
+connection.connect();
+
+
+// =============================================================
 
 //get a single product item
-app.get('/test', function (req, res) {
+app.get('/test' , function (req, res) {
 
-    res.status(200).json({
-        "success": true,
-        "message": "product deleted successfully"
-    })
 
+
+    connection.query('select * from test where 1'
+        , [null]
+        , (error, results, fields) => {
+
+            //data base unknow error
+            if (error) {
+                res.status(500).send(
+                    {
+                        "success": false,
+                        "message": "somthing went wrong"
+                    }
+                );
+                return false;
+            }
+
+            //check if product not exists
+            if (results.length == 0) {
+                res.status(200).json({
+                    "success": false,
+                    "message": "product not found"
+                })
+                return false;
+            }
+
+            res.status(200).json({
+                "success": true,
+                "message": "product deleted successfully",
+                "product": results
+            })
+
+            // ======================================
+        });
 })
 // =============================================================
 
