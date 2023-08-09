963890bb4710c43060fa668a26cba2320ccd6b40
hamzaougjjou <hamzaougjjou04@gmail.com>
2023-08-09 07:12:42 +0100

    edite auth user

--- controllers/SellerController.js
+++ controllers/SellerController.js
@@ -22,7 +22,7 @@ app.post('/seller/create', function (req, res) {
 
     let id = generateId();
     let { name, phone, password } = req.query;
-    let userId = auth.user.id ;    //validate data 
+    let userId = auth.user.id;    //validate data 
     //check for a valid name
     if (!name) {
         res.status(400).send(
@@ -51,7 +51,7 @@ app.post('/seller/create', function (req, res) {
             }
         );
         return 0;
-    } else if ( password.length < 6 ) {
+    } else if (password.length < 6) {
         res.status(400).send(
             {
                 "success": false,
@@ -61,17 +61,17 @@ app.post('/seller/create', function (req, res) {
         return 0;
     }
 
-    if ( !phone ) {
+    if (!phone) {
         phone = null;
     }
-   
+
     let role = 'seller';
-    if ( auth.user.role.toLocaleLowerCase() == "seller") {
+    if (auth.user.role.toLocaleLowerCase() == "seller") {
         role = "client";
     }
 
     connection.query('INSERT INTO users (id,name,phone,password,role,created_by) VALUES ( ? , ?  , ? , ? , ? , ? )'
-        , [id, name.trim(), phone, password , role , userId]
+        , [id, name.trim(), phone, password, role, userId]
         , (error, results, fields) => {
             if (error) {
                 res.status(500).send(
@@ -93,5 +93,110 @@ app.post('/seller/create', function (req, res) {
                 }
             })
         });
+
+})
+
+//update a auth user
+app.put('/auth/:id', function (req, res) {
+    const auth = authenticateToken(req);
+    //check if user loged in
+    if (auth.status === false) {
+        res.status(401).json({
+            "success": false,
+            "message": "unauthenticated",
+        });
+        return false;
+    }
+    if (auth.user.role != "admin" && auth.user.role != "seller") {
+        res.status(401).send(
+            {
+                "success": false,
+                "message": "unautherized"
+            }
+        );
+        return false;
+    }
+
+    let userId = req.params.id;
+    let name = req.query.name;
+    let password = req.query.password;
+    let phone = req.query.phone;
+
+    if (!name || name.trim().length < 1) {
+        res.status(500).send(
+            {
+                "success": false,
+                "message": "invalid name"
+            }
+        );
+        return false;
+    }
+
+    if (!password || password.trim().length < 6) {
+        res.status(200).send(
+            {
+                "success": false,
+                "message": "invalid password"
+            }
+        );
+        return false;
+    }
+
+
+    let parameters = [name, password, userId];
+    let q = "update users set name=? , password=? where id=?";
+
+    if (phone) {
+        q = "update users set name=? , password=? , phone=? where  id=? ";
+        parameters = [name, password, phone, userId];
+    }
+
+
+    connection.query("select * from users where ( id=? and password=? ) "
+        ,
+        [userId, password]
+        , (error1, results1, fields1) => {
+            //data base unknow error
+            if (error1) {
+                res.status(500).send(
+                    {
+                        "success": false,
+                        "message": "somthing went wrong"
+                    }
+                );
+                return false;
+            }
+            if (results1.length == 0) {
+
+                res.status(200).send(
+                    {
+                        "success": false,
+                        "message": "password not correct"
+                    }
+                );
+                return false;
+            }
+            connection.query(q, parameters
+                , (error2, results2, fields3) => {
+                    //data base unknow error
+                    if (error2) {
+                        res.status(500).send(
+                            {
+                                "success": false,
+                                "message": "somthing went wrong"
+                            }
+                        );
+                        return false;
+                    }
+
+                    res.status(200).json({
+                        "success": true,
+                        "message": "user updated successfully",
+                        "result": results2
+                    })
+
+                });
+        })
+
 
 })
--- index.js
+++ index.js
@@ -24,6 +24,7 @@ var connection = mysql.createConnection({
     password: "jBbszbEUjWB9d9tn9Hy9",
     database: "b8urbhjjqawd9w3ukybw"
 });
+
 connection.connect();
 
 // create a new post
@@ -512,7 +513,7 @@ app.put('/user/:id', function (req, res) {
         });
         return false;
     }
-    if (auth.user.role != "admin") {
+    if (auth.user.role != "admin" && auth.user.role != "seller" ) {
         res.status(401).send(
             {
                 "success": false,
@@ -536,6 +537,7 @@ app.put('/user/:id', function (req, res) {
         );
         return false;
     }
+
     if ( !password || password.trim().length < 6 ) {
         res.status(500).send(
             {
@@ -618,6 +620,110 @@ app.get('/products', function (req, res) {
         });
 })
 
+//update a auth user
+app.put('/auth/:id', function (req, res) {
+    const auth = authenticateToken(req);
+    //check if user loged in
+    if (auth.status === false) {
+        res.status(401).json({
+            "success": false,
+            "message": "unauthenticated",
+        });
+        return false;
+    }
+    if (auth.user.role != "admin" && auth.user.role != "seller") {
+        res.status(401).send(
+            {
+                "success": false,
+                "message": "unautherized"
+            }
+        );
+        return false;
+    }
+
+    let userId = req.params.id;
+    let name = req.query.name;
+    let password = req.query.password;
+    let phone = req.query.phone;
+
+    if (!name || name.trim().length < 1) {
+        res.status(500).send(
+            {
+                "success": false,
+                "message": "invalid name"
+            }
+        );
+        return false;
+    }
+
+    if (!password || password.trim().length < 6) {
+        res.status(200).send(
+            {
+                "success": false,
+                "message": "invalid password"
+            }
+        );
+        return false;
+    }
+
+
+    let parameters = [name, password, userId];
+    let q = "update users set name=? , password=? where id=?";
+
+    if (phone) {
+        q = "update users set name=? , password=? , phone=? where  id=? ";
+        parameters = [name, password, phone, userId];
+    }
+
+
+    connection.query("select * from users where ( id=? and password=? ) "
+        ,
+        [userId, password]
+        , (error1, results1, fields1) => {
+            //data base unknow error
+            if (error1) {
+                res.status(500).send(
+                    {
+                        "success": false,
+                        "message": "somthing went wrong"
+                    }
+                );
+                return false;
+            }
+            if (results1.length == 0) {
+
+                res.status(200).send(
+                    {
+                        "success": false,
+                        "message": "password not correct"
+                    }
+                );
+                return false;
+            }
+            connection.query(q, parameters
+                , (error2, results2, fields3) => {
+                    //data base unknow error
+                    if (error2) {
+                        res.status(500).send(
+                            {
+                                "success": false,
+                                "message": "somthing went wrong"
+                            }
+                        );
+                        return false;
+                    }
+
+                    res.status(200).json({
+                        "success": true,
+                        "message": "user updated successfully",
+                        "result": results2
+                    })
+
+                });
+        })
+
+
+})
 
 //create a new product by user
 app.post('/product/create', function (req, res) {
@@ -730,7 +836,6 @@ app.post('/product/create', function (req, res) {
         });
 
 })
-
 
 // // delete product
 app.put('/product/:id', function (req, res) {
